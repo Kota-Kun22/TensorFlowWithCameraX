34e6796870c0231223cf91381e5871dfae448748
Harsh Rastogi <harshrastogi013@gmail.com>
2024-05-15 00:17:23 +0530

    initial commit along with dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,87 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.example.tensorflowlitecamerax"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.tensorflowlitecamerax"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+
+
+    implementation(libs.androidx.camera.core)//implementation("androidx.camera:camera-core:1.3.3")
+    implementation(libs.androidx.camera.camera2)//implementation("androidx.camera:camera-camera2:1.3.3")
+    implementation(libs.androidx.camera.lifecycle)//implementation("androidx.camera:camera-lifecycle:1.3.3")
+    implementation(libs.androidx.camera.video)//implementation("androidx.camera:camera-video:1.3.3")
+
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.extensions)
+
+    implementation(libs.tensorflow.lite.task.vision) //implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.0")
+    implementation(libs.tensorflow.lite.gpu.delegate.plugin)//implementation("org.tensorflow:tensorflow-lite-gpu-delegate-plugin:0.4.0")
+    implementation(libs.tensorflow.lite.gpu)   //implementation("org.tensorflow:tensorflow-lite-gpu:2.9.0")
+
+
+
+
+}
